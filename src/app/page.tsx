7de34e2d9787b2735d4edd5c1fe042e4125dbf92
141AQUA-xzy <sudeshsatpute0@gmail.com>
2024-10-08 13:43:22 +0530

    Added talk-tok folder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,62 @@
-import Image from "next/image";
+"use client";
+import { useEffect, useState } from "react";
+import { io } from "socket.io-client";
+import CategoryIcon from "@mui/icons-material/Category";
+import { Chat } from "./Chat";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const socket = io("https://talk-tok-backendd.onrender.com"); // Initialize the socket connection on client-side
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [username, setUsername] = useState("");
+  const [roomID, setId] = useState("");
+  const [showChat, setVisibility] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  const JoinRoom = () => {
+    if (username === "" || roomID === "") {
+      toast("Username & RoomID are needed");
+      return;
+    }
+    if (username !== "" && roomID !== "") {
+      socket.emit("join_room", roomID);
+    }
+    setVisibility(true);
+  };
+
+  return (
+    <main id="main" className="bg-custom-conic h-lvh w-['100%'] flex flex-col">
+      <ToastContainer />
+
+      <div className="flex flex-row items-center gap-1 text-lg p-5 uxll:text-4xl">
+        <CategoryIcon />
+        <span className="uppercase text-purple-500 font-extrabold">
+          Talk-Tok
+        </span>
+      </div>
+      <div className="flex flex-col m-7 gap-2 items-center justify-center uxll:pt-60 uxlll:pt-96">
+        {showChat ? (
+          <Chat socket={socket} username={username} room={roomID} />
+        ) : (
+          <div className="bg-white/20 flex flex-col items-center backdrop-blur-lg rounded-lg p-6 max-w-md my-auto mx-auto">
+            <input
+              placeholder="Username"
+              className="p-1 border rounded-md uxll:h-16 uxll:w-72 m-2 text-black"
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+            <input
+              placeholder="RoomID"
+              className="p-1 border rounded-md uxll:h-16 uxll:w-72 text-black"
+              onChange={(e) => setId(e.target.value)}
+            ></input>
+            <button
+              onClick={JoinRoom}
+              className="bg-red-400 rounded-xl uppercase p-2 text-black font-bold uxll:h-8 uxll:w-40 m-2 hover:scale-x-105 hover:-translate-y-1 transition-all ease-in"
+            >
+              Connect
+            </button>
+          </div>
+        )}
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        "custom-conic":
+          "conic-gradient(from 36deg at 20% 80%, #A100FFFF 0%, #000000 25%, #0000 30%)",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      screens: {
+        'uxll': '1700px',
+        'uxlll': '1930px',
+
       },
     },
   },
