4730f1f0c176046be653c723450c91c366da2cbc
Aqvenor <119067911+141AQUA-xzy@users.noreply.github.com>
2024-10-09 05:00:49 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://talk-tok-backendd.onrender.com",
     methods: ["GET", "POST"],
   },
 });
